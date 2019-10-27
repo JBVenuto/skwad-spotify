4c3423cb5d9ed3631788378231efe307559195e4
Joseph Venuto <jbvenuto@gmail.com>
2019-10-27 19:07:04 -0400

    Log the relevant data

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ var spotify = new Spotify(keys.spotify);
 
 spotify.search({
     type: 'track',
-    query: 'stranger than fiction'
+    query: 'welcome to paradise'
 }, function(err, res) {
     if(err){
-        console.log(err);
+        console.log('error: ', err);
     } 
     else{
-        console.log(res)
+        var data = res.tracks.items;
+        for (i = 0; i < data.length; i++){
+            console.log('artist:', data[i].artists[0].name);
+            console.log('song:', data[i].name);
+            console.log('album:', data[i].album.name);
+            console.log('url:', data[i].external_urls.spotify);
+            console.log('\n');
+        }
     }
 })
